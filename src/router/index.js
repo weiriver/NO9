cddd77d9b97d3905ec5f31c681666ddf8fcf03bb
张伟江 <weijiang.zhang@genlot.com>
2018-12-24 20:13:17 +0800

    布局

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import station from './station'
-import sysSetup from './sysSetup'
+// import station from './station'
+// import sysSetup from './sysSetup'
 import {HMF} from './base'
 
-const Desktop = () => import('@/views/desktop/Index')
-const Login = () => import('@/views/login/Index')
-const Refresh = () => import('@/views/subindex/Refresh')
-const PrintPage = () => import('@/views/subindex/PrintPage')
-const PrintStatementPage = () => import('@/views/subindex/PrintStatement')
+// const Desktop = () => import('@/views/desktop/Index')
+// const Login = () => import('@/views/login/Index')
+// const Refresh = () => import('@/views/subindex/Refresh')
+// const PrintPage = () => import('@/views/subindex/PrintPage')
+// const PrintStatementPage = () => import('@/views/subindex/PrintStatement')
 const test = () => import('@/views/subindex/test')
 
 Vue.use(Router)
@@ -24,33 +24,33 @@ export default new Router({
         {path: 'abc', component: test}
       ]
     },
-    {
-      path: '/desktop',
-      name: 'desktop',
-      component: Desktop
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: Login
-    },
-    {
-      path: '/refresh',
-      name: 'Refresh',
-      component: Refresh
-    },
-    {
-      path: '/printPage',
-      name: 'PrintPage',
-      component: PrintPage
-    },
-    {
-      path: '/printStatementPage',
-      name: 'printStatementPage',
-      component: PrintStatementPage
-    },
-    ...station,
-    ...sysSetup,
+    // {
+    //   path: '/desktop',
+    //   name: 'desktop',
+    //   component: Desktop
+    // },
+    // {
+    //   path: '/login',
+    //   name: 'login',
+    //   component: Login
+    // },
+    // {
+    //   path: '/refresh',
+    //   name: 'Refresh',
+    //   component: Refresh
+    // },
+    // {
+    //   path: '/printPage',
+    //   name: 'PrintPage',
+    //   component: PrintPage
+    // },
+    // {
+    //   path: '/printStatementPage',
+    //   name: 'printStatementPage',
+    //   component: PrintStatementPage
+    // },
+    // ...station,
+    // ...sysSetup,
     {
       path: '*',
       redirect: '/a' // 默认跳转到登录
